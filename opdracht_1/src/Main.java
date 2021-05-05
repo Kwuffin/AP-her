7d7f5cbc6fc2965b4d2d31f57c1026731ccc0600
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-05 16:51:58 +0200

    Wrote code to append animals to their respective stalls. Business rule: A stall can't have more than five animals

--- opdracht_1/src/Main.java
+++ opdracht_1/src/Main.java
@@ -4,51 +4,104 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Create stalls
+        Kippenhok kippenhok = new Kippenhok("Kippenhok 1");
+        Paardenstal paardenstal = new Paardenstal("Paardenstal 1");
+        Koeienstal koeienstal = new Koeienstal("Koeienstal 1");
 
         while (true) {
-            Scanner sc = new Scanner(System.in);
+
+            // User can create their own animals
             System.out.println("Welk dier wil je maken?\n" +
                     "Keuze uit: 'Kip', 'Paard', 'Koe'");
 
             String animal_input = sc.nextLine(); // Read user input
 
             if (animal_input.equalsIgnoreCase("kip")) {
+
+                // User input for a name
                 System.out.println("Geef je kip een naam:");
                 String naam_input = sc.nextLine();
 
+                // User input for a color
                 System.out.println("Geef je kip een kleur:");
                 String kleur_input = sc.nextLine();
 
+                // User input for an age
                 System.out.println("Geef je kip een leeftijd:");
                 int leeftijd_input = sc.nextInt();
 
+                // If chicken stall is full, don't create animal
+                if (kippenhok.getKippen().size() >= 5){
+                    System.out.println("Kippenhok zit vol!");
+                    continue;
+                }
+
+                // Create animal
                 Kip user_kip = new Kip(naam_input, kleur_input, leeftijd_input);
+
+                // Append animal to stall
+                kippenhok.add_kip(user_kip);
             }
 
             else if (animal_input.equalsIgnoreCase("paard")) {
+
+                // User input for a name
                 System.out.println("Geef je paard een naam:");
                 String naam_input = sc.nextLine();
 
+                // User input for a color
                 System.out.println("Geef je paard een kleur:");
                 String kleur_input = sc.nextLine();
 
+                // User input for an age
                 System.out.println("Geef je paard een leeftijd:");
                 int leeftijd_input = sc.nextInt();
 
+                // If horsestall is full, don't create animal
+                if (paardenstal.getPaarden().size() >= 5){
+                    System.out.println("Paardenstal zit vol!");
+                    continue;
+                }
+
+                // Create animal
                 Paard user_paard = new Paard(naam_input, kleur_input, leeftijd_input);
+
+                // Append animal to stall
+                paardenstal.add_paard(user_paard);
             }
 
             else if (animal_input.equalsIgnoreCase("koe")) {
+                // User input for a name
                 System.out.println("Geef je koe een naam:");
                 String naam_input = sc.nextLine();
 
+                // User input for a color
                 System.out.println("Geef je koe een kleur:");
                 String kleur_input = sc.nextLine();
 
+                // User input for an age
                 System.out.println("Geef je koe een leeftijd:");
                 int leeftijd_input = sc.nextInt();
 
+                // If cowstall is full, don't create animal
+                if (koeienstal.getKoeien().size() >= 5){
+                    System.out.println("Koeienstal zit vol!");
+                    continue;
+                }
+
+                // Create animal
                 Koe user_koe = new Koe(naam_input, kleur_input, leeftijd_input);
+
+                // Append animal to stall
+                koeienstal.add_koe(user_koe);
+            }
+
+            else {
+                System.out.println("Ongeldige invoer!");
+                continue;
             }
 
 
@@ -101,6 +154,5 @@ public class Main {
 //        System.out.println(koe1.maak_geluid() + "\n");
 
         }
-
     }
 }
