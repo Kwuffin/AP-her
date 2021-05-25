41c56733ebfe43ac817ffb3d8aeb1d7ca94a7072
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-25 22:37:17 +0200

    Added attributes and multipliers

--- opdracht_3/src/Rit.java
+++ opdracht_3/src/Rit.java
@@ -1,2 +1,17 @@
 public class Rit {
+    Double multiplier;
+    String name;
+
+    public Rit(String name) {
+        this.name = name;
+        this.multiplier = 1.0;
+    }
+
+    public Double getMultiplier() {
+        return multiplier;
+    }
+
+    public String toString(){
+        return String.format("This is ride '%s'", name);
+    }
 }
--- opdracht_3/src/Trein.java
+++ opdracht_3/src/Trein.java
@@ -1,2 +1,17 @@
 public class Trein {
+    Double multiplier;
+    String name;
+
+    public Trein(String name) {
+        this.name = name;
+        this.multiplier = 1.2;
+    }
+
+    public Double getMultiplier() {
+        return multiplier;
+    }
+
+    public String toString(){
+        return String.format("This is train '%s'", name);
+    }
 }
