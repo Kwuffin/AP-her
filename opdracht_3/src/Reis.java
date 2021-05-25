488cb14c2caad5faaabaef1fee97e95f27929753
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-26 01:08:08 +0200

    Added getters and setters

--- opdracht_3/src/Reis.java
+++ opdracht_3/src/Reis.java
@@ -1,2 +1,26 @@
-public class Reis {
+import java.util.*;
+
+public class Reis extends Graph {
+    private Node end_node;  // Destination node
+    private List<Stap> steps = new ArrayList<>();  // Steps to get to destination node
+
+
+
+
+    // Getters and setters
+    public Node getEnd_node() {
+        return end_node;
+    }
+
+    public void setEnd_node(Node end_node) {
+        this.end_node = end_node;
+    }
+
+    public List<Stap> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Stap> steps) {
+        this.steps = steps;
+    }
 }
