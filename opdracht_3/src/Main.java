3d582e567780b9253df5b663c91a5cc8d1a6957f
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-26 22:32:23 +0200

    Fine-tuned the distances

--- opdracht_3/src/Main.java
+++ opdracht_3/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     private static void Test()
     {
         Travel_Method ride = new Travel_Method("car ride",1.00);
-        Travel_Method train_ride = new Travel_Method("train ride", 1.5);
+        Travel_Method train_ride = new Travel_Method("train ride", 1.3);
         Travel_Method flight = new Travel_Method("flight", 3.00);
 
         // Create nodes
@@ -20,25 +20,25 @@ public class Main {
         Node nodeF = new Node("Node F");
 
         // Set node attributes
-        nodeA.addDestination(nodeB, 10.00);
+        nodeA.addDestination(nodeB, 100.0);
         nodeA.add_Travel_Method(nodeB, ride);
-        nodeA.addDestination(nodeC, 30.00);
+        nodeA.addDestination(nodeC, 50.0);
         nodeA.add_Travel_Method(nodeC, flight);
 
-        nodeB.addDestination(nodeD, 30.00);
-        nodeB.add_Travel_Method(nodeD, train_ride);
-        nodeB.addDestination(nodeF, 20.00);
-        nodeB.add_Travel_Method(nodeF,ride);
+        nodeB.addDestination(nodeD, 40.0);
+        nodeB.add_Travel_Method(nodeD, flight);
+        nodeB.addDestination(nodeF, 70.0);
+        nodeB.add_Travel_Method(nodeF, train_ride);
 
-        nodeC.addDestination(nodeE, 15.00);
+        nodeC.addDestination(nodeE, 45.0);
         nodeC.add_Travel_Method(nodeE, train_ride);
 
-        nodeD.addDestination(nodeF,2.00);
+        nodeD.addDestination(nodeF,10.0);
         nodeD.add_Travel_Method(nodeF, ride);
-        nodeD.addDestination(nodeE,5.00);
+        nodeD.addDestination(nodeE,20.0);
         nodeD.add_Travel_Method(nodeE, ride);
 
-        nodeF.addDestination(nodeE,15.00);
+        nodeF.addDestination(nodeE,50.0);
         nodeF.add_Travel_Method(nodeE, ride);
 
         // Add Nodes to graph
