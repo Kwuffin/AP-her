4724451c8229c62a80ce7651b0860bd10115caa4
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-26 15:38:56 +0200

    Set more node attributes and a way to print the route. Total Distance doesn't work yet

--- opdracht_3/src/Main.java
+++ opdracht_3/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        Test();
+
+    }
+
+    private static void Test()
+    {
+        Travel_Method ride = new Travel_Method("car rice",1.00);
+        Travel_Method train_ride = new Travel_Method("train ride", 1.5);
+        Travel_Method flight = new Travel_Method("flight", 3.00);
+
+        // Create nodes
         Node nodeA = new Node("A");
         Node nodeB = new Node("B");
         Node nodeC = new Node("C");
@@ -7,29 +19,50 @@ public class Main {
         Node nodeE = new Node("E");
         Node nodeF = new Node("F");
 
-        nodeA.addDestination(nodeB, 10);
-        nodeA.addDestination(nodeC, 15);
+        // Set node attributes
+        nodeA.addDestination(nodeB, 10.00);
+        nodeA.add_Travel_Method(nodeB, ride);
+        nodeA.addDestination(nodeC, 30.00);
+        nodeA.add_Travel_Method(nodeC, flight);
 
-        nodeB.addDestination(nodeD, 12);
-        nodeB.addDestination(nodeF, 15);
+        nodeB.addDestination(nodeD, 30.00);
+        nodeB.add_Travel_Method(nodeD, train_ride);
+        nodeB.addDestination(nodeF, 20.00);
+        nodeB.add_Travel_Method(nodeF,ride);
 
-        nodeC.addDestination(nodeE, 10);
+        nodeC.addDestination(nodeE, 15.00);
+        nodeC.add_Travel_Method(nodeE, train_ride);
 
-        nodeD.addDestination(nodeE, 2);
-        nodeD.addDestination(nodeF, 1);
+        nodeD.addDestination(nodeF,2.00);
+        nodeD.add_Travel_Method(nodeF, ride);
+        nodeD.addDestination(nodeE,5.00);
+        nodeD.add_Travel_Method(nodeE, ride);
 
-        nodeF.addDestination(nodeE, 5);
+        nodeF.addDestination(nodeE,15.00);
+        nodeF.add_Travel_Method(nodeE, ride);
 
+        // Add Nodes to graph
         Graph graph = new Graph();
-
         graph.addNode(nodeA);
         graph.addNode(nodeB);
         graph.addNode(nodeC);
         graph.addNode(nodeD);
-        graph.addNode(nodeE);
         graph.addNode(nodeF);
+        graph.addNode(nodeE);
 
-        graph = Dijkstra.calculateShortestPathFromSource(graph, nodeA);
-        System.out.println(graph.getNodes());
+        Node start_node = nodeA;  // Source node TODO: Make user variable in the future
+        Node end_node = nodeE;  // Destination node TODO: Make user variable in the future
+        graph =  Dijkstra.calculateShortestPathFromSource(graph, start_node);
+
+        Reis reis = new Reis(nodeE);
+        reis.setSteps(reis);
+
+        System.out.println("Total distance: "+reis.getDistance()+"\n");
+        System.out.println("Route:");
+        for (Node nd : end_node.getShortestPath()) {
+            System.out.println(nd.toString());
+        }
+
+        System.out.println(end_node.toString());
     }
 }
